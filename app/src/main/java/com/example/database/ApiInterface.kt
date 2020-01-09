d2a42737bff570829787090941d613fb026722ea
sonayeroyan <sona.yeroyan@picsart.com>
2020-01-09 21:51:20 +0400

    .

--- app/src/main/java/com/example/database/ApiInterface.kt
+++ app/src/main/java/com/example/database/ApiInterface.kt
@@ -8,7 +8,7 @@ import retrofit2.http.GET
 
 interface ApiInterface {
 
-    @GET("/search?q=12%20years%20a%20slave&format=json&tag=film/film,tone/reviews&from-date=2010-01-01&show-tags=contributor&show-fields=starRating,headline,thumbnail,short-url&order-by=relevance&api-key=test")
+    @GET("/search?q=12%20years%20a%20slave&format=json&tag=film/film,tone/reviews&from-date=2010-01-01&show-tags=contributor&show-fields=starRating,headline,thumbnail,short-url&order-by=relevance&api-key=da3e84a8-a10c-403d-9817-863e081ee713")
     fun getArticles(): Call<List<Article>>
 
     companion object {
